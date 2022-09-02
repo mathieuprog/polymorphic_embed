056c63562a2b90c695e86266ded98de9d2544b46
Aaron Renner <aaron.renner@gmail.com>
2022-09-02 07:59:27 -0600

    Export polymorphic_embeds_* as locals_with_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,14 @@
 # Used by "mix format"
+locals_with_parens = [
+  polymorphic_embeds_one: 2,
+  polymorphic_embeds_many: 2
+]
+
 [
   import_deps: [:ecto],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_with_parens,
+  export: [
+    locals_with_parens: locals_with_parens
+  ]
 ]
