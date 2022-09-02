027be4fcf1c090e19c7163f7b041a1eafbb34640
Aaron Renner <aaron.renner@gmail.com>
2022-09-02 08:40:14 -0600

    Fix typos

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
-locals_with_parens = [
+locals_without_parens = [
   polymorphic_embeds_one: 2,
   polymorphic_embeds_many: 2
 ]
@@ -7,8 +7,8 @@ locals_with_parens = [
 [
   import_deps: [:ecto],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: locals_with_parens,
+  locals_without_parens: locals_without_parens,
   export: [
-    locals_with_parens: locals_with_parens
+    locals_without_parens: locals_without_parens
   ]
 ]
