15cdb770dd3b3cd4d8e9dd849f39254d1a489038
Alex Naser <alex.naser@remote.com>
2024-02-12 21:19:40 +0100

    Update ecto

--- mix.exs
+++ mix.exs
@@ -47,11 +47,11 @@ defmodule PolymorphicEmbed.MixProject do
 
   defp deps do
     [
-      {:ecto, "~> 3.9"},
+      {:ecto, "~> 3.11"},
       {:jason, "~> 1.4"},
       {:phoenix_html, "~> 2.14 or ~> 3.2", optional: true},
       {:ex_doc, "~> 0.28", only: :dev},
-      {:ecto_sql, "~> 3.9", only: :test},
+      {:ecto_sql, "~> 3.11", only: :test},
       {:postgrex, "~> 0.16", only: :test},
       {:query_builder, "~> 1.0", only: :test},
       {:phoenix_ecto, "~> 4.4", only: :test},
